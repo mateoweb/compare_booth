202225b4c79f83af67a1b1cd57eda6aa3e01d000
Mateo <mateo.webdev@gmail.com>
2019-07-12 09:27:34 +0200

    Uptdate Last commit

---
+++ compare_booth/src/components/FilteredResultBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FilteredResultBox = () => {
+        return ( 
+            <div>
+
+            </div>
+         );
+}
+ 
+export default FilteredResultBox;
---
+++ compare_booth/src/components/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class SearchBox extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <React.Fragment>
+                
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default SearchBox;
