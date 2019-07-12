012665a0b0b36f27e2e31c37372198665e155ce7
Mateo <mateo.webdev@gmail.com>
2019-07-12 13:02:39 +0200

    Test Airbnb module

--- compare_booth/src/App.js
+++ compare_booth/src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import './App.css';
-import SearchBox from './components/SearchBox'
-import data from './dataShop.json'
+import React, { Component } from "react";
+import "./App.css";
+import SearchBox from "./components/SearchBox";
+import dataShop from "./dataShop.json";
+import 'rheostat/initialize';
 
-function App() {
-  const loadData = data;
-  console.log(loadData);
-  return (
-    <div className="App">
-    <header>
-    <SearchBox/>
-    </header>
+const data = dataShop[0].data;
+const id = dataShop[0].data[0].id;
 
+console.log(id);
+
+class App extends Component {
+  state = {
+    data: {data}
+  };
+  
+  
+  render() {
+   // {this.state.data.map((person, index) => (
+    //  <p key={index}>Hello, {person.name} from {person.country}!</p>
+ // ))}
+    
+  
+    return (
+      <div className="App">
+        <header>
+          <SearchBox />
+        </header>
+
+        <div>
+   
     </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ compare_booth/src/components/slider_airbnb.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import Rheostat from 'rheostat';
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<Rheostat />, document.getElementById('slider-root'));
