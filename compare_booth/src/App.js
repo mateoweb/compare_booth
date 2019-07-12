b168dfff74a03954132ed403b9352dc85b0f05c5
Mateo <mateo.webdev@gmail.com>
2019-07-12 15:35:49 +0200

    Filter by filter() method

--- compare_booth/src/App.js
+++ compare_booth/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import Shop from './components/Shop'
 //import SearchBox from "./components/SearchBox";
+import dataShop from './dataShop.json'
 
   class App extends Component {
     state = {  }
+
+    onHandleClick = () => {
+      const dataFilter = dataShop.filter(dataShop => dataShop.Cabine_photo === "OUI")
+      console.log(dataFilter);
+      
+    }
+
     render() { 
       return ( 
         <div>
+        <button onClick={this.onHandleClick}>Test Button</button>
         <Shop/>
         </div>
        );
--- compare_booth/src/components/Shop.jsx
+++ compare_booth/src/components/Shop.jsx
@@ -2,15 +2,18 @@ import React, { Component } from 'react'
 import dataShop from '../dataShop'
 
 const data = dataShop;
+
+
+
 class Shop extends Component {
     state = {  }
 
-    
 
     render() { 
         console.log(data)
         return ( 
             <div>
+          
             { dataShop.map((detail, index)=>{
                 return <h1 key={index}>{detail.Nom}</h1>                
             }) }
