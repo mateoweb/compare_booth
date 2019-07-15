6b2efad14c1225c41280cb5227aa24962d9751ad
Mateo <mateo.webdev@gmail.com>
2019-07-15 18:01:54 +0200

    FilterbyMateriel Working

--- compare_booth/src/App.js
+++ compare_booth/src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import Shop from './components/Shop'
+//import Shop from './components/Shop'
 //import SearchBox from "./components/SearchBox";
-import dataShop from './dataShop.json'
+
 import Filters from './components/Filters'
 
   class App extends Component {
     state = {  }
 
-    filterClick = () => {
-      //utiliser find() pour trouver un seul élément
-      //utiliser filter() pour trouver plusieurs éléments et créer un nouveau
-
-      const dataFilter = dataShop.filter(dataShop => (dataShop.Cabine_photo === "NON") && (dataShop.Helio_booth === "OUI"))
-      console.log(dataFilter);
-      
-    }
+   
 
     render() { 
       return ( 
         <div>
         <Filters/>
-        <button onClick={this.filterClick}>Test Button</button>
-        <Shop/>
+        
         </div>
        );
     }
--- compare_booth/src/components/Filters.jsx
+++ compare_booth/src/components/Filters.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import dataShop from '../dataShop.json'
 
 class Filters extends React.Component {
   constructor(props) {
@@ -20,6 +21,49 @@ class Filters extends React.Component {
     const value = input.type === "checkbox" ? input.checked : input.value;
     this.setState({ [name]: value });
   };
+
+  filterClick = (e) => {
+       e.preventDefault();
+    //utiliser find() pour trouver un seul élément
+    //utiliser filter() pour trouver plusieurs éléments et créer un nouveau
+    if (this.state.cabinePhoto === true){
+        const dataFilter = dataShop.filter(dataShop => (dataShop.Borne_photo === "NON") && (dataShop.Cabine_photo === "OUI") && (dataShop.Helio_booth === "NON"))
+        console.log(dataFilter);
+    }
+    else if (this.state.bornePhoto === true){
+        const dataFilter2 = dataShop.filter(dataShop => (dataShop.Borne_photo === "OUI") && (dataShop.Cabine_photo === "NON") && (dataShop.Helio_booth === "NON"))
+        console.log(dataFilter2);
+    }
+    else if (this.state.helioBooth === true){
+        const dataFilter3 = dataShop.filter(dataShop => (dataShop.Helio_booth === "OUI") && (dataShop.Borne_photo === "NON") && (dataShop.Cabine_photo === "NON"))
+        console.log(dataFilter3);
+    }
+    else if (this.state.cabinePhoto === true && this.state.bornePhoto === true){
+        const dataFilter4 = dataShop.filter(dataShop => (dataShop.Cabine_photo === "OUI") && (dataShop.Borne_photo === "OUI") && (dataShop.Helio_booth === "NON"))
+        console.log(dataFilter4);
+    }
+    else if (this.state.cabinePhoto === true && this.state.helioBooth === true){
+        const dataFilter5 = dataShop.filter(dataShop => (dataShop.Cabine_photo === "OUI") && (dataShop.Helio_booth === "OUI") && (dataShop.Borne_photo === "NON"))
+        console.log(dataFilter5);
+    }
+    else if (this.state.cabinePhoto === true && this.state.helioBooth === true && this.state.bornePhoto === true){
+        const dataFilter6 = dataShop.filter(dataShop => (dataShop.Cabine_photo === "OUI") && (dataShop.Borne_photo === "OUI") && (dataShop.Helio_booth === "OUI"))
+        console.log(dataFilter6);
+    }
+    else if (this.state.bornePhoto === true && this.state.helioBooth === true){
+        const dataFilter7 = dataShop.filter(dataShop => (dataShop.Cabine_photo === "NON") && (dataShop.Helio_booth === "OUI") && (dataShop.Borne_photo === "OUI"))
+        console.log(dataFilter7);
+    }
+    else if (this.state.cabinePhoto === true && this.state.helioBooth === true){
+        const dataFilter8 = dataShop.filter(dataShop => (dataShop.Cabine_photo === "OUI") && (dataShop.Helio_booth === "OUI") && (dataShop.Borne_photo === "NON"))
+        console.log(dataFilter8);
+    }
+    else {
+        console.log('please select an option');
+    }
+    
+    
+  }
 
   render() {
     return (
@@ -68,22 +112,23 @@ class Filters extends React.Component {
             type="checkbox"
             value={this.state.cabinePhoto}
             onChange={this.handleChanges}
-          />Cabine photo:{" "}
+          />Cabine photo{" "}
           
           <input
             name="bornePhoto"
             type="checkbox"
             value={this.state.bornePhoto}
             onChange={this.handleChanges}
-          />Borne photo:{" "}
+          />Borne photo{" "}
           
           <input
             name="helioBooth"
             type="checkbox"
             value={this.state.helioBooth}
             onChange={this.handleChanges}
-          />Héliobooth:{" "}
+          />Héliobooth{" "}<br/>
         </label>
+        <button onClick={this.filterClick}>Go !</button>
       </form>
     );
   }
